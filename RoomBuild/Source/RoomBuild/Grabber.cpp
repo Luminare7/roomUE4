913bcb8ac2d1806f8e7c7b92a0441dee76e532fb
Luminare7 <amos@amosbocelli.com>
2022-02-22 16:45:55 +0100

    upto_120

--- RoomBuild/Source/RoomBuild/Grabber.cpp
+++ RoomBuild/Source/RoomBuild/Grabber.cpp
@@ -1,7 +1,11 @@
 // Amos Bocelli Copyrights
 
-
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -18,8 +22,7 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
 	
 }
 
@@ -29,6 +32,33 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player viewpoint:
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s."),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString()
+	);
+	// Logging out to test
+
+	// Draw a line from player showing the reach:
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0.f,
+		5.f
+	);
+	// Ray-Cast out to a certain distance ( Reach)
 }
 
